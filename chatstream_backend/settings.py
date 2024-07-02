bbcec93bebe6f32f73eccc2c1da666f446496ae6
harshal-509 <sharma.harshal509@gmail.com>
2024-07-02 15:15:03 +0530

    modified settings

--- chatstream_backend/settings.py
+++ chatstream_backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(' ')
+ALLOWED_HOSTS = ['*']
 
 
 
