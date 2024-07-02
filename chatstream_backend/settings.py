e5ff185456a796d580e479249754cf958756d4b9
harshal-509 <sharma.harshal509@gmail.com>
2024-07-02 14:44:28 +0530

    updated settings.py

--- chatstream_backend/settings.py
+++ chatstream_backend/settings.py
@@ -28,10 +28,13 @@ if os.path.isfile(dotenv_file):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(' ')
+
 
 
 # Application definition
